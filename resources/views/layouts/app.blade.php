5b904565b3d9483ed84a8dcf306cb994e5f357a1
Ian Patel <hello@ian-patel.com>
2018-03-07 23:26:49 +1300

    Fix Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <title>Blog</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
         <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
         <!-- 
          ==================================

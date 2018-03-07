3db34bb50529c3ad0ed65c152c2dbfd6193575c9
Ian Patel <hello@ian-patel.com>
2018-03-07 22:50:32 +1300

    Add Frontend blade layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+        <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+        <!-- 
+         ==================================
+            Made with ♥ around the world
+            Created by @IanPatel
+            hello@ian-patel.com
+        ==================================
+        -->
+    </head>
+    
+    <body class="feed-all bootstrapped halfView">
+        <div id="app" v-cloak> 
+            @yield('content')
+        </div>
+    </body>
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+</html>
